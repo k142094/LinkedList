b7cbc2fe0e7304cc42a6208c3b885c330e70c3e4
k142094 <k142094@nu.edu.pk>
2015-08-27 13:51:08 +0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,26 @@
+public class Node
+{
+	private Node next;
+	private Integer data;
+	public Node()
+	{
+		this.next=null;
+		this.data=null;
+	}
+	public Integer getData(void)
+	{
+		return this.data;
+	}
+	public void setData(Integer param)
+	{
+		this.data=param;
+	}
+	public Node getNext(void)
+	{
+		this.data=param;
+	}
+	protected void  setNext(Node param)
+	{
+		this.data=param;
+	}
+}
